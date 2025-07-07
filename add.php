d85c2ea34a4c6dc51d318cc70cf3543ded4e5ebe
myy16 <myy155116@gmail.com>
2025-07-07 17:56:23 +0300

    (refactor) move add logic to add.php

--- add.php
+++ add.php
@@ -1,3 +1,26 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $title = trim($_POST['title']);
+    $content = trim($_POST['content']);
+
+    if ($title !== '' && $content !== '') {
+        $newNote = [
+            'title' => htmlspecialchars($title),
+            'content' => htmlspecialchars($content),
+            'date' => date("Y-m-d H:i:s"),
+        ];
+
+        if (file_exists('notes.json')) {
+            $notes = json_decode(file_get_contents('notes.json'), true);
+        }
+
+        $notes[] = $newNote;
+        file_put_contents('notes.json', json_encode($notes, JSON_PRETTY_PRINT));
+    }
+    header("Location:" . $_SERVER['PHP_SELF']);
+    exit;
+}
+?>
 <!DOCTYPE html>
 <html>
 
--- index.php
+++ index.php
@@ -1,28 +1,7 @@
 <?php
 $notes = [];
 
-// Not kaydetme işlemi
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $title = trim($_POST['title']);
-    $content = trim($_POST['content']);
 
-    if ($title !== '' && $content !== '') {
-        $newNote = [
-            'title' => htmlspecialchars($title),
-            'content' => htmlspecialchars($content),
-            'date' => date("Y-m-d H:i:s"),
-        ];
-
-        if (file_exists('notes.json')) {
-            $notes = json_decode(file_get_contents('notes.json'), true);
-        }
-
-        $notes[] = $newNote;
-        file_put_contents('notes.json', json_encode($notes, JSON_PRETTY_PRINT));
-    }
-    header("Location:" . $_SERVER['PHP_SELF']);
-    exit;
-}
 
 // Notları oku
 if (file_exists('notes.json')) {
