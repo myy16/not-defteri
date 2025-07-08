c867abcb413ea29d1fd55365bd74e5aef7872bcc
myy16 <myy155116@gmail.com>
2025-07-08 14:29:21 +0300

    (feat) edit.php : add redirection

--- edit.php
+++ edit.php
@@ -41,7 +41,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
 
         </form>
-
+           <a class="btn btn-primary mt-3" href="index.php">Go Back</a>
     </div>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.7/js/bootstrap.min.js" integrity="sha512-zKeerWHHuP3ar7kX2WKBSENzb+GJytFSBL6HrR2nPSR1kOX1qjm+oHooQtbDpDBSITgyl7QXZApvDfDWvKjkUw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
