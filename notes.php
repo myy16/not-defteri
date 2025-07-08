533fc7cc1a67d14374a22e916091b29eba719acd
myy16 <myy155116@gmail.com>
2025-07-08 09:23:57 +0300

    (fix) notes.php : relocate add button

--- notes.php
+++ notes.php
@@ -26,13 +26,15 @@
             <?php endforeach; ?>
     </div>
     
-        <a class="btn btn-primary mt-3" href="add.php">Add Note</a>
+       
     
         
 <?php else: ?>
     <p>Do not add yet any note</p>
 <?php endif; ?>
 
+ <a class="btn btn-primary mt-3" href="add.php">Add Note</a>
+ 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.7/js/bootstrap.min.js" integrity="sha512-zKeerWHHuP3ar7kX2WKBSENzb+GJytFSBL6HrR2nPSR1kOX1qjm+oHooQtbDpDBSITgyl7QXZApvDfDWvKjkUw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 </body>
