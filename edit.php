a900276d749680c98efc767d2df21c5a98b0e47c
myy16 <myy155116@gmail.com>
2025-07-08 14:26:31 +0300

    (feat) edit.php :  genarate edit menu

--- edit.php
+++ edit.php
@@ -8,8 +8,12 @@ if (file_exists($jsonFile)) {
     $notes = json_decode(file_get_contents($jsonFile), true);
 }
 
+$note = $notes[$id];
 
-
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $title = trim($_POST['title']);
+    $content = trim($_POST['content']);
+}
 ?>
 
 
@@ -26,14 +30,21 @@ if (file_exists($jsonFile)) {
 <body>
     <div class="container">
         <h1>Edit Notes</h1>
+        <form method="POST">
+            <label for="title" class="form-label">Title:</label>
+            <input type="text" name="title" placeholder="Title">
 
-        
-        
-    </form>
+            <label for="content" class="form-label">Note:</label>
+            <textarea name="content" placeholder="Content"></textarea>
+
+            <button type="submit" class="btn btn-primary">Submit</button>
+
+
+        </form>
 
     </div>
 
-     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.7/js/bootstrap.min.js" integrity="sha512-zKeerWHHuP3ar7kX2WKBSENzb+GJytFSBL6HrR2nPSR1kOX1qjm+oHooQtbDpDBSITgyl7QXZApvDfDWvKjkUw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.7/js/bootstrap.min.js" integrity="sha512-zKeerWHHuP3ar7kX2WKBSENzb+GJytFSBL6HrR2nPSR1kOX1qjm+oHooQtbDpDBSITgyl7QXZApvDfDWvKjkUw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 </body>
 
