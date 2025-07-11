f22add98723d58bdb3c7e78bafb95090fb434591
myy16 <myy155116@gmail.com>
2025-07-11 09:23:32 +0300

    (refactor) head.php

--- notes.php
+++ notes.php
@@ -9,47 +9,44 @@ if (file_exists('notes.json')) {
 <!DOCTYPE html>
 <html>
 
-<head>
-    <meta charset="UTF-8">
-    <title>Notes</title>
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.7/css/bootstrap.min.css" integrity="sha512-fw7f+TcMjTb7bpbLJZlP8g2Y4XcCyFZW8uy8HsRZsH/SwbMw0plKHFHr99DN3l04VsYNwvzicUX/6qurvIxbxw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="style.css">
-</head>
+<?php
+$title = "Notes";
+include "head.php";
+?>
 
 <body>
     <div class="container">
         <div class="header-flex">
-    <h1>Notes</h1>
-    <div class="text-xl-center mb-3">
-        <a class="btn btn-primary fw-bold fs-3" href="add.php">+</a>
-    </div>
-    </div>
-    <div class="notes-container ">
-        
-        <?php if (!empty($notes)): ?>
-            <?php foreach ($notes as $id => $note): ?>
-                <div class="card ">
-                    <div class="card-header d-flex justify-content-between">
+            <h1>Notes</h1>
+            <div class="text-xl-center mb-3">
+                <a class="btn btn-primary fw-bold fs-3" href="add.php">+</a>
+            </div>
+        </div>
+        <div class="notes-container ">
 
-                        <div>
-                            <?= $note['title'] ?>
-                            (<?= $note['date'] ?>)
+            <?php if (!empty($notes)): ?>
+                <?php foreach ($notes as $id => $note): ?>
+                    <div class="card mb-3">
+                        <div class="card-header d-flex justify-content-between">
+
+                            <div>
+                                <?= $note['title'] ?>
+                                (<?= $note['date'] ?>)
+                            </div>
+                            <div class="btn-right">
+                                <a class="btn btn-secondary btn-sm me-2" href="edit.php?id=<?= $id ?>">Edit Note</a>
+                                <a class="btn btn-danger btn-sm" href="delete.php?id=<?= $id ?>">Delete Note</a>
+                            </div>
                         </div>
-                        <div class="btn-right">
-                            <a class="btn btn-secondary btn-sm me-2" href="edit.php?id=<?= $id ?>">Edit Note</a>
-                            <a class="btn btn-danger btn-sm" href="delete.php?id=<?= $id ?>">Delete Note</a>
+                        <div class="card-body">
+                            <?= nl2br($note['content']) ?>
                         </div>
                     </div>
-                    <div class="card-body">
-                        <?= nl2br($note['content']) ?>
-                    </div>
-                </div>
-            <?php endforeach; ?>
+                <?php endforeach; ?>
+        </div>
+
+
     </div>
-
-
-</div>
 
 <?php else: ?>
     <p>Do not add yet any note</p>
