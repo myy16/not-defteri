01ea7f78dca39eb7146432ac098505510f632f4a
myy16 <myy155116@gmail.com>
2025-07-11 15:07:39 +0300

    (refactor) head.php

---
+++ footer.php
@@ -0,0 +1,15 @@
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.min.js"></script>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        var TitleElements = document.querySelectorAll('[title]');
+        for (var TitleElement of TitleElements) {
+
+            var tooltip = new bootstrap.Tooltip(TitleElement);
+        }
+
+    });
+</script>
--- notes.php
+++ notes.php
@@ -52,7 +52,7 @@ include "head.php";
     <p>Do not add yet any note</p>
 <?php endif; ?>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.7/js/bootstrap.min.js" integrity="sha512-zKeerWHHuP3ar7kX2WKBSENzb+GJytFSBL6HrR2nPSR1kOX1qjm+oHooQtbDpDBSITgyl7QXZApvDfDWvKjkUw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<?php include "footer.php"; ?>
 
 </body>
 
