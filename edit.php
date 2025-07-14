7c6010ea5a54344fbe3ee8ad87028135f0e0b8e9
myy16 <myy155116@gmail.com>
2025-07-14 15:05:45 +0300

    (refactor) add templates/edit.php

--- edit.php
+++ edit.php
@@ -33,73 +33,27 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         }
     }
 }
-?>
-
-
-<!DOCTYPE html>
-<html lang="tr">
-
-<?php
 $title = "Edit Note";
-include "head.php";
-?>
 
-<body>
-    <div class="container">
+$datas = [
+    'title' => "Edit Note",
+    'buttons' => [
+        [
+            'href' => 'index.php',
+            'class' => 'btn btn-primary fw-bold fs-4',
+            'text' => 'Notes',
+            'title' => 'Back to Notes'
+        ],
+        [
+            'type' => 'submit',
+            'form_id' => 'edit_form',
+            'class' => 'btn btn-danger fw-bold fs-4',
+            'text' => 'Save',
+            'title' => 'Save Note'
+        ]
+    ]
+];
 
-        <?php
+$form_id = 'edit_form';
 
-        $datas = [
-            'title' => "Edit Note",
-            'buttons' => [
-                [
-                    'href' => 'index.php',
-                    'class' => 'btn btn-primary fw-bold fs-4',
-                    'text' => 'Notes',
-                    'title' => 'Back to Notes'
-                ],
-                [
-                    'type' => 'submit',
-                    'form_id' => 'edit_form',
-                    'class' => 'btn btn-danger fw-bold fs-4',
-                    'text' => 'Save',
-                    'title' => 'Save Note'
-                ]
-            ]
-        ];
-
-        $form_id = 'edit_form';
-        include "header.php" ?>
-
-        <br><br><br><br>
-        <form method="POST" id="<?= $form_id ?>">
-            <?php if (!empty($error)): ?>
-                <div class="alert alert-danger">
-                    <?php echo htmlspecialchars($error); ?>
-                </div>
-            <?php endif; ?>
-
-            <?php if ($showSuccessModal): ?>
-                <div class="alert alert-success text-center">
-                    <h5> ✅ Successfully!</h5>
-                    Your note updated!
-                </div>
-            <?php endif; ?>
-
-            <div class="mb-3">
-                <label for="title" class="form-label">Title:</label>
-                <input name='title' type="text" class="form-control" id="title" value="<?php echo htmlspecialchars($note['title']); ?>" placeholder="Title">
-            </div>
-            <br>
-            <div class="mb-3">
-                <label for="content" class="form-label">Note:</label>
-                <textarea name="content" class="form-control" id="content" rows="3" placeholder="Content"><?php echo htmlspecialchars($note['content']); ?></textarea>
-            </div>
-        </form>
-    </div>
-
-    <?php include "footer.php"; ?>
-
-</body>
-
-</html>
+include "templates/edit.php";
---
+++ templates/edit.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="tr">
+
+<?php include "head.php"; ?>
+
+<body>
+    <div class="container">
+
+        <?php include "header.php" ?>
+
+        <br><br><br><br>
+        <form method="POST" id="<?= $form_id ?>">
+            <?php if (!empty($error)): ?>
+                <div class="alert alert-danger">
+                    <?php echo htmlspecialchars($error); ?>
+                </div>
+            <?php endif; ?>
+
+            <?php if ($showSuccessModal): ?>
+                <div class="alert alert-success text-center">
+                    <h5> ✅ Successfully!</h5>
+                    Your note updated!
+                </div>
+            <?php endif; ?>
+
+            <div class="mb-3">
+                <label for="title" class="form-label">Title:</label>
+                <input name='title' type="text" class="form-control" id="title" value="<?php echo htmlspecialchars($note['title']); ?>" placeholder="Title">
+            </div>
+            <br>
+            <div class="mb-3">
+                <label for="content" class="form-label">Note:</label>
+                <textarea name="content" class="form-control" id="content" rows="3" placeholder="Content"><?php echo htmlspecialchars($note['content']); ?></textarea>
+            </div>
+        </form>
+    </div>
+
+    <?php include "footer.php"; ?>
+
+</body>
+
+</html>
