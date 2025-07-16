b0e729b3bfda39c51a4b8f3d9120165029c68035
myy16 <myy155116@gmail.com>
2025-07-16 16:48:16 +0300

    (refactor) add smarty.php

--- pages/notes.php
+++ pages/notes.php
@@ -1,17 +1,6 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-// put full path to Smarty.class.php
-require __DIR__ . '/../vendor/autoload.php'; // Adjust the path as necessary
-$smarty = new Smarty\Smarty();
-
-$smarty->setTemplateDir('../templates');
-$smarty->setCompileDir('../templates_c');
-// $smarty->setCacheDir('/web/www.example.com/smarty/cache');
-// $smarty->setConfigDir('/web/www.example.com/smarty/configs');
-
-
+require '../smarty.php';
 
 $notes = [];
 
---
+++ smarty.php
@@ -0,0 +1,12 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require __DIR__ . '/vendor/autoload.php';
+$smarty = new Smarty\Smarty();
+
+$smarty->setTemplateDir('../templates');
+$smarty->setCompileDir('../templates_c');
+// $smarty->setCacheDir('/web/www.example.com/smarty/cache');
+// $smarty->setConfigDir('/web/www.example.com/smarty/configs');
