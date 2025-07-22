999c1ad1e84d5c426a8e0ad59ae457dcb95e7b9e
myy16 <myy155116@gmail.com>
2025-07-22 10:15:16 +0300

    (refactor) add slim

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
 <?php
-chdir("pages");
-include '../pages/notes.php';
 
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require __DIR__ . '/vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello world!");
+    return $response;
+});
+
+$app->run();
