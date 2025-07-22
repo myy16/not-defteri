2845df4919c204b74ce6192e28b82ae209f6f731
myy16 <myy155116@gmail.com>
2025-07-22 13:54:35 +0300

    (chore) dashboard.php : add title

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -8,5 +8,6 @@ if (file_exists('notes.json')) {
     $notes = json_decode(file_get_contents('notes.json'), true);
 }
 
+$smarty->assign('title', 'Dashboard');
 $smarty->assign('notes', $notes);
 $smarty->display('pages/dashboard.tpl');
