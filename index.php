00ba2ba76fe142bbb1b3c864b55cbae30de79673
myy16 <myy155116@gmail.com>
2025-07-24 17:21:02 +0300

    (chore) index.php : reorder

--- index.php
+++ index.php
@@ -5,16 +5,17 @@ use Slim\Factory\AppFactory;
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-require "smarty.php";
-
 require __DIR__ . '/vendor/autoload.php';
-require __DIR__ . '/controllers/NotesController.php';
-require __DIR__ . '/controllers/HomeController.php';
+
 require __DIR__ . '/helpers.php';
+require __DIR__ . '/smarty.php';
+require __DIR__ . '/controllers/HomeController.php';
+require __DIR__ . '/controllers/NotesController.php';
 
 $app = AppFactory::create();
 
 $errorMiddleware = $app->addErrorMiddleware(true, true, true);
 
 include 'routes.php';
+
 $app->run();
